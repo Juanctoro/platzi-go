711148db0fefbc664b41737adfc87cb9d6205665
Juan Toro <juancarlostorocifuentes12@gmail.com>
2024-09-12 10:32:03 -0500

    Segundo commit, funcion que multiplica por 2

--- Go/main.go
+++ Go/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func multiplicacion(a int) int {
+	return a * 2
+}
+
 func main() {
 	fmt.Println("Primer commit")
+
+	fmt.Println("Para el segundo commit voy a hacer una funcion que calcule el doble de un valor")
+	fmt.Println(multiplicacion(3))
 }
